2f3673c5f1294d904a4a1ae99f4a6460d25a5188
Melodi Bayraktar <101207246+MelodiBayraktar@users.noreply.github.com>
2023-08-15 13:38:37 +0300

    first-commit

---
+++ calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/Users/melodibayraktar/Desktop/RustCalculator/calculator/target/debug/calculator: /Users/melodibayraktar/Desktop/RustCalculator/calculator/src/main.rs
---
+++ calculator/target/debug/deps/calculator-08bf6f1b4278bc01.d
@@ -0,0 +1,5 @@
+/Users/melodibayraktar/Desktop/RustCalculator/calculator/target/debug/deps/calculator-08bf6f1b4278bc01: src/main.rs
+
+/Users/melodibayraktar/Desktop/RustCalculator/calculator/target/debug/deps/calculator-08bf6f1b4278bc01.d: src/main.rs
+
+src/main.rs:
